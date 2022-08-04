b4f696b8e2f3c1a5e6f657a2fcee438b6f8b2bc5
Suleiman Ali <suleimanali.business@gmail.com>
2022-08-04 07:52:09 +0300

    Fix typo

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -36,7 +36,7 @@ function Intro(): JSX.Element {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 80 }}
+        transition={{ delay: 0.8 }}
       >
         <Arrow className="intro__arrow" />
       </motion.div>
